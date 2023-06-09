38b705b2465b367664fbac17244b4a4ee43890ea
sooto selim <adewaleselim6@gmail.com>
2023-06-09 16:15:57 +0100

    first commit

---
+++ c.md
@@ -0,0 +1,12 @@
+VARIABLE
+
+to declare a varialble in c++, first we have to specify the kind of data we want to store eg
+
+    #int for interger number
+
+then the name of the variable
+#int user
+then the value of the variable
+#int user = selim
+
+<!-- storirng decimal point  -->
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    int file_size = 100;
+    const double sales = 9.99;
+    sales = 0;
+    std::cout << sales;
+    // int a = 1;
+    // int b = 2;
+    // int temp = b;
+    // b = a;
+    // a = temp;
+    // std::cout << a;
+    return 0;
+}
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     cout<<"Hello World";
+//     return 0;
+// }
